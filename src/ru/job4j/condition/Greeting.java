b3783eea862f017ec3306e8c990351c375543606
Foneom <mugoloka@gmail.com>
2020-04-30 15:43:10 +0300

    9.2. Checkstyle[#281480]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,10 +2,10 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea="I like Java!";
-        System.out.println(idea+" But I am a newbie.");
-        int year=2020;
-        String id=idea+year;
+        String idea = "I like Java!";
+        System.out.println(idea + " But I am a newbie.");
+        int year = 2020;
+        String id = idea + year;
         System.out.println(id);
     }
 }
