a363480c630c40f9c7cc5735bbee28d70c1a8a8b
Foneom <mugoloka@gmail.com>
2020-04-30 20:00:00 +0300

    0. Что такое тестирование.[#281500]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -7,13 +7,29 @@ public class Converter {
     }
 
     public static int rubleToDollar(int value1) {
-        int rsl = value1/60;
+        int rsl = value1 / 60;
         return rsl;
+    }
+    public static void testRubleToEuro() {
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+    }
+    public static void testRubleToDollar() {
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("180 rubles are 3. Test result : " + passed);
     }
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(180);
+        Converter.testRubleToEuro();
+        Converter.testRubleToDollar();
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("180 rubles are " + dollar + " dollar.");
     }
