78b88d2be70899fbb38d1e9d3080819b847070da
Foneom <mugoloka@gmail.com>
2020-05-05 01:03:35 +0300

    5.8. JavaDoc. Code convention[#281526]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,36 @@
 package ru.job4j.converter;
 
+/**
+ * Class Converter для конвертации валюты
+ * @author kboriskin
+ * @since 05.05.2020
+ * @version 1
+ */
 public class Converter {
+    /**
+     * конвертация рубля в евро
+     * @param value
+     * @return
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
+
+    /**
+     * конвертация рубля в доллар
+     * @param value1
+     * @return
+     */
 
     public static int rubleToDollar(int value1) {
         int rsl = value1 / 60;
         return rsl;
     }
 
+    /**
+     * тест метода rubleToEuro
+     */
     public static void testRubleToEuro() {
         int in = 140;
         int expected = 2;
@@ -18,7 +38,9 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
     }
-
+    /**
+     * тест метода rubleToDollar
+     */
     public static void testRubleToDollar() {
         int in = 140;
         int expected = 2;
@@ -26,6 +48,11 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("180 rubles are 3. Test result : " + passed);
     }
+
+    /**
+     * Main
+     * @param args
+     */
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
