7b2b0b86f538f135184961f49e8f6ec51c14d27e
Foneom <mugoloka@gmail.com>
2020-05-06 12:50:40 +0300

    5.9. Крест в псевдографике. [#281519]

--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -8,21 +8,8 @@ import static org.junit.Assert.*;
 public class CheckPrimeNumberTest {
 
     @Test
-    public void when5() {
-        PrimeNumber prime = new PrimeNumber();
-        int count = prime.calc(5);
-        assertThat(count, is(3));
-    }
-    @Test
-    public void when11() {
-        PrimeNumber prime = new PrimeNumber();
-        int count = prime.calc(11);
-        assertThat(count, is(5));
-    }
-    @Test
-    public void when2() {
-        PrimeNumber prime = new PrimeNumber();
-        int count = prime.calc(2);
-        assertThat(count, is(1));
+    public void check5IsPrime() {
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
     }
 }
