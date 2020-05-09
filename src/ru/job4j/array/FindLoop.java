74376bab8aa65d80feec0535af78f6bc349fff46
Foneom <mugoloka@gmail.com>
2020-05-09 11:49:20 +0300

    6.5. Поиск индекса в диапазоне.[#281447]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,12 +1,24 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public int indexOf (int[] data, int el) {
+    public int indexOff (int[] data, int el) {
         int rst = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 rst = i;
                 break;
+            }
+        }
+        return rst;
+    }
+
+    public static int indexOf (int[] date, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = 0; i < date.length; i++) {
+            for (int j = start; j <= finish; j++) {
+                if (date[i] == el && date[i] == date[j]) {
+                    return el;
+                }
             }
         }
         return rst;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -12,7 +12,7 @@ public class FindLoopTest {
         FindLoop findLoop = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int rslt = findLoop.indexOf(input, value);
+        int rslt = findLoop.indexOff(input, value);
         int expect = 0;
         assertThat(rslt, is(expect));
     }
@@ -21,9 +21,32 @@ public class FindLoopTest {
         FindLoop findLoop = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 6;
-        int rslt = findLoop.indexOf(input, value);
+        int rslt = findLoop.indexOff(input, value);
         int expect = 0;
         assertThat(rslt, is(expect));
     }
 
+    @Test
+    public void findElement(){
+        FindLoop findLoop = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 3;
+        int start = 0;
+        int finish = 2;
+        int result = findLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void notFindElement(){
+        FindLoop findLoop = new FindLoop();
+        int[] input = new int[] {5, 10, 3, 2, 1, 4};
+        int value = 5;
+        int start = 1;
+        int finish = 5;
+        int result = findLoop.indexOf(input, value, start, finish);
+        int expect = 5;
+        assertThat(result, is(expect));
+    }
 }
