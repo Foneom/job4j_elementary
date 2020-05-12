65280d04fe69dccfb3ac047890e7650d211d6f07
Foneom <mugoloka@gmail.com>
2020-05-12 15:19:30 +0300

    5.9. Крест в псевдографике. [#281519]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,12 +2,12 @@ package ru.job4j.array;
 
 public class ArrayChar {
 
-    public static boolean startsWith (char[] word, char[] pref) {
+    public static boolean startsWith(char[] word, char[] pref) {
         for (int i = 0; i < word.length; i++) {
-                if (word[0] == pref[0] && word[1] == pref[1]){
-                    return true;
-                }
+            if (word[i] == pref[i] && word[i + 1] == pref[i + 1]) {
+                return true;
             }
-        return false;
+        }
+        return true;
     }
 }
--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -4,16 +4,13 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row == cell && size == 3 || row + cell == 2 && size == 3;
-                boolean right = (row + cell) == 4 && size == 5 || row == cell && size == 5;
+                boolean left = (row + cell) == 2 * row;
+                boolean right = (row + cell) == (size - 1);
                 if (left) {
                     System.out.print("0");
-                }
-
-                if (right) {
+                } else if (right) {
                     System.out.print("0");
-                }
-                else {
+                } else {
                     System.out.print(" ");
                 }
             }
