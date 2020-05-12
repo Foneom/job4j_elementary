48837d64aa2e8f4798e3cdba5d2b5fd1808e6bbe
Foneom <mugoloka@gmail.com>
2020-05-12 16:58:50 +0300

    Слово начинается с ...[#281442]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,11 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
-        for (int i = 0; i < word.length; i++) {
-            if (word[i] == pref[i] && word[i + 1] == pref[i + 1]) {
-                return true;
-            }
-        }
-        return true;
+        boolean result = true;
+        for (int i = 0; i < word.length; i++)
+            for (int j = 0; j < pref.length; j++)
+                if (word[i] == pref[j]) {
+                    return true;
+                }
+        return false;
     }
 }
