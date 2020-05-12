e7642b7ad5afd5ebc2adec12e6ef8072d8365095
Foneom <mugoloka@gmail.com>
2020-05-12 13:24:08 +0300

    5.9. Крест в псевдографике. [#281519]

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -1,23 +1,25 @@
 package ru.job4j.loop;
 
 public class Slash {
-    public static void draw (int size) {
+    public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row % 2 == 0 && (row + cell) % 2 == 0;
-                boolean right = cell % 2 != 0 && (row + cell) % 2 == 0 && (row - cell) != 2 && (cell - row) != 2;
-
+                boolean left = row == cell && size == 3 || row + cell == 2 && size == 3;
+                boolean right = (row + cell) == 4 && size == 5 || row == cell && size == 5;
                 if (left) {
-                    System.out.print("0");
-                } else if (right) {
-                    System.out.print("0");
-                } else {
+                    System.out.print("*");
+                }
+
+                if (right) {
+                    System.out.print("*");
+                }
+                else {
                     System.out.print(" ");
                 }
             }
-                System.out.println();
-            }
+            System.out.println();
         }
+    }
 
     public static void main (String[] args) {
         System.out.println("Draw by 3");
