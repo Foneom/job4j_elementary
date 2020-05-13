615e74a8fc15d9b7c7509cdd8567050f8457b7be
Foneom <mugoloka@gmail.com>
2020-05-13 23:11:45 +0300

    6.4.1. Слово заканчивается на ...[#281446]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,14 +2,14 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        for (int i = 0; i < word.length; i++) {
-            for (int j = 0; j < post.length; j++) {
-                if (word[i] == post[j]) {
-                    return true;
-                }
+        boolean result = true;
+        for (int j = 0; j < post.length; j++) {
+            if (word[(word.length - 1) - j] != post[(post.length -1) - j]) {
+                result = false;
+                break;
             }
         }
-        return false;
+        return result;
     }
 }
 
