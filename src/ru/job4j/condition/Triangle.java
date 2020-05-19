635ef99f8700a55308f1b431868f5cec294c2dc7
Foneom <mugoloka@gmail.com>
2020-05-19 19:26:30 +0300

    2.2. Рефакторинг - Площадь треугольника.[#281604]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,9 +1,40 @@
 package ru.job4j.condition;
 
+import ru.job4j.condition.TrqArea;
 public class Triangle {
 
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ab, Point ac, Point bc) {
+        this.first = ab;
+        this.second = ac;
+        this.third = bc;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        if (this.exist(a, b, c)) {
+            rsl = TrqArea.square(a, b, c);
+            }
+        return rsl;
+        }
+
+    public static boolean exist(double a, double b, double c) {
+        boolean exs = a + b > c && b + c > a && a + c > b;
+        if (exs) {
+            return true;
+        }
+        return false;
+    }
+    /**
     public static boolean exist(double ab, double ac, double bc) {
         boolean can = ab + ac > bc && ac + bc > ab && ab + bc > ac;
         return can;
     }
+     */
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -11,5 +13,16 @@ public class TriangleTest {
     public void whenExist() {
         boolean result = Triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
+    }
+    @Test
+    public void whenTriangleExistAreaTest() {
+        Point a = new Point(1, 1);
+        Point b = new Point(5, 1);
+        Point c = new Point(5, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 6;
+        double out = triangle.area();
+        Assert.assertEquals(expected, out, 0.01);
+
     }
 }
